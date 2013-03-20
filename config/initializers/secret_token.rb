9809012fb6c7b86108cb1f1c0280d5a48db84a24
sonyfe25cp <sonyfe25cp@gmail.com>
2013-03-21 00:07:11 +0800

    init the web

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrueLoser::Application.config.secret_token = 'b8e486cbdc07924249d815bdc617a6e9a11cae684685be331cfd2cb8d4f06e42afdababa223ed74589d6ed4680161ee5a469477183aefc063c0b7de4ac2fb1a3'
